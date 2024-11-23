a87a19a3b1e0077bba9d7370d1d8c64f83d457e6
Conor Sheridan <c.sheridan30@universityofgalway.ie>
2024-11-23 19:59:50 +0000

    keiming initialisation function

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import numpy as np
+import matplotlib as mpl
+
+def initialize(self, inputs: int, outputs: int):
+
+    standardDeviation = np.sqrt(2.0/inputs)
+    weights = np.random.normal(loc=0.0, scale=standardDeviation, size=(inputs,outputs))
+
+    biases = np.zeros(outputs)
+    return self(weights,biases)
+
+class InputData:
+
+    features:np.ndarray
+    labels:np.ndarray
+    numFeatures:int
 
 def main():
     pass
