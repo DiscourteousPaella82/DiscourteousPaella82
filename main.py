0fff693b291df2594f9ed54399b11355af532fea
Conor Sheridan <c.sheridan30@universityofgalway.ie>
2024-11-24 11:10:51 +0000

    Changed Project to Tensorflow Pandas. Using Mace Head weather station data

--- main.py
+++ main.py
@@ -1,55 +1,29 @@
+import pandas as pd
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import tensorflow as tf
 import numpy as np
+
 import matplotlib as mpl
 
-class Layer:
 
-    weights: np.ndarray
-    biases: np.ndarray
-
-    def initialize(self, inputs: int, outputs: int):
-
-        standard_deviation = np.sqrt(2.0/inputs)
-        weights = np.random.normal(loc=0.0, scale=standard_deviation, size=(inputs,outputs))
-
-        biases = np.zeros(outputs)
-        return self(weights,biases)
-
-class Network:
-
-    hidden1: Layer
-    hidden2: Layer
-    hidden3: Layer
-    output: Layer
-
-class InputData:
-    features:np.ndarray
-    labels:np.ndarray
-    numFeatures:int
-
-def load_data(filename:str):
-    with open(filename,'r') as f:
-        pass    #load labels
-
-def load_labels(filename:str):
-    with open(filename,'r') as f:
-        pass    #load labels
-
-def normalize_data():
-    features = np.max(np.absolute(features), axis=0)    #normalizes data between -1 and 1
-
-def relu(x: np.ndarray):
-    return np.maximum(0, x) #sets any value less than 0 to 0
-
-def softmax(x:np.ndarray):
-    exponential_x = np.exp(x - np.max(x, axis = 1, keepdims=True))
-    return exponential_x / np.sum(exponential_x, axis = 1, keepdims=True)
-
-def predict(input: np.ndarray):
-    output = softmax(input)
-    return np.argmax(output, axis=1)
+#Mace Head Weather Station daily data from 1st October 2024 to 30th October 2024
+#model needs to predict maximum air temperature given features
 
 def main():
-    pass
+    weather_train = pd.read_csv("dly275.csv",
+     names=["Maximum Air Temperature", "Minimum  Air Temperature", "09utc Grass Minimum Temperature", "Mean 10cm soil temperature",
+               "Mean CBL Pressure", "Mean Wind Speed", "Highest ten minute mean wind speed", "Wind Direction at max 10 min mean", "Highest Gust",
+            "Potential Evapotranspiration", "Evaporation", "Soil Moisture Deficits(mm) well drained",
+            "Soil Moisture Deficits(mm) moderately drained", "Soil Moisture Deficits(mm) poorly drained", "Global Radiation"])
+
+    weather_train.head()
+
+    weather_features = weather_train.copy()
+    weather_labels = weather_features.pop('Maximum Air Temperature')
+
+    weather_features = np.array(weather_features)
+    weather_features
 
 if __name__ == '__main__':
  main()
